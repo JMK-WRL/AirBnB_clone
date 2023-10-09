250dae03cdbe89a0361d0669dce0914ebf58aee9
Jonathan <jonathankyule2@gmail.com>
2023-10-09 12:21:15 -0700

    basemodel update

---
+++ models/base_model.py
@@ -0,0 +1,19 @@
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        """Initializes BaseModel instance."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+
+
